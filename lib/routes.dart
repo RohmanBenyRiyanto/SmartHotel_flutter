4786822bf53323b4884ff0ed087f0173c64f8a4c
Reva Rahardian <rahardianreva123@gmail.com>
2021-11-06 14:36:22 +0700

    Reva

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:smart_hotel/home/home.dart';
+import 'package:smart_hotel/sign/forgot_password/new_password.dart';
+import 'package:smart_hotel/sign/forgot_password/password_hint.dart';
 import 'package:smart_hotel/sign/forgot_password/search_account.dart';
 import 'package:smart_hotel/sign/sign_in/sign_in.dart';
 import 'package:smart_hotel/sign/sign_up/sign_up.dart';
@@ -13,4 +15,6 @@ final Map<String, WidgetBuilder> routes = {
   SignUp.routeName: (context) => SignUp(),
   SearchAccount.routeName: (context) => SearchAccount(),
   Home.routeName: (context) => Home(),
+  PasswordHint.routeName: (context) => PasswordHint(),
+  NewPassword.routeName: (context) => NewPassword(),
 };
