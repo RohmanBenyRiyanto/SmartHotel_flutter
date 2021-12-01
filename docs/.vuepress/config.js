e309567b32b4724f749b4370e1014d8886631bf8
Restu Indrawan <57904667+restuindrawan@users.noreply.github.com>
2021-12-01 15:26:54 +0800

    Add docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,109 @@
+module.exports = {
+    lang: 'en-US',
+    title: 'Smotel',
+    description: 'Documentation of Smotel',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: 'https://raw.githubusercontent.com/RohmanBenyRiyanto/SmartHotel_flutter/main/assets/images/logo.png'
+        }]
+    ],
+
+    themeConfig: {
+        logo: 'https://raw.githubusercontent.com/RohmanBenyRiyanto/SmartHotel_flutter/main/assets/images/logo.png',
+        navbar: [
+            // NavbarItem
+            {
+                text: 'Github',
+                link: 'https://github.com/RohmanBenyRiyanto/SmartHotel_flutter',
+            },
+            // string - page file path
+            //   '/bar/README.md',
+        ],
+        sidebar: [
+            // SidebarItem
+            {
+                text: 'Intro',
+                // link: '/home-page/',
+                children: [
+                    // SidebarItem
+                    {
+                        text: 'Apa itu Smotel?',
+                        link: '../README.md',
+                    },
+                ],
+            },
+            // SidebarItem
+            {
+                text: 'Dokumentasi',
+                // link: '/home-page/',
+                children: [
+                    {
+                        text: 'Splash Screen',
+                        link: '/splash/splash.md',
+                    },
+                    // SidebarItem
+                    {
+                        text: 'Authentication',
+                        children: [{
+                                text: 'Login',
+                                link: '/auth-page/login.md',
+                            },
+                            {
+                                text: 'Register',
+                                link: '/auth-page/register.md',
+                            },
+                            {
+                                text: 'Lupa Password',
+                                link: '/auth-page/forgot.md',
+                            },
+                        ],
+                    },
+                    {
+                        text: 'Home',
+                        children: [{
+                                text: 'Navbar',
+                                link: '/home-page/navbar.md',
+                            },
+                            {
+                                text: 'Home',
+                                link: '/home-page/home-page.md',
+                            },
+                            {
+                                text: 'History',
+                                link: '/home-page/myroom.md',
+                            },
+                            {
+                                text: 'MyScan',
+                                link: '/home-page/myscan.md',
+                            },
+                            {
+                                text: 'MyRoom',
+                                link: '/home-page/history.md',
+                            },
+                            {
+                                text: 'Profile',
+                                link: '/home-page/profile.md',
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                text: 'Credit',
+                // link: '/home-page/',
+                children: [
+                    // SidebarItem
+                    {
+                        text: 'Package',
+                        link: '/credit/package.md',
+                    },
+                    {
+                        text: 'Team',
+                        link: '/credit/team.md',
+                    },
+                ],
+            },
+        ],
+    },
+}
